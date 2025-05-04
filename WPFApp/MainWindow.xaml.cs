b180d4238a2afa51ded4e28aaa87d542d1de725e
Samir Agudelo Curaca <samiragudelo21@gmail.com>
2025-05-03 19:07:56 -0500

    Expresion y operacion Lambda Task T5

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -32,7 +32,24 @@ namespace WPFApp
                 MessageBox.Show("Ejecutando el metodo anonimo.");
             }
             );
+            Task T3 = new Task(
+                () => ShowMesage());
+
+            // Una expresion Lambda:
+            // (aqui viene los parametros de entrada) => Espresion
+            // () => Expresion
+            // el operador Lambda (=>) se lee como "va hacia"
+
+            Task T4 = new Task(()=> MessageBox.Show("Ejecutando la tarea 4"));
+            Task T5 = new Task(() =>
+            {
+                DateTime CurrentDate = DateTime.Today;
+                DateTime StartDate = CurrentDate.AddDays(30);
+                MessageBox.Show($"Tarea 5. Fecha Calculada: {StartDate}");
+            }
+            );
         }
+        
         void ShowMesage()
         {
             MessageBox.Show("Ejecutando el metodo ShowMessege");
